893e4a45c53b83ba203a3b5c8ccc27fdc399091f
chen <1243104437@qq.com>
2022-08-19 21:39:00 +0800

    初始化02

--- src/main/java/com/gec/Day0815SpringBootHazardousApplication.java
+++ src/main/java/com/gec/Day0815SpringBootHazardousApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Day0815SpringBootHazardousApplication {
 
     public static void main(String[] args) {
+        System.out.println("你好");
         SpringApplication.run(Day0815SpringBootHazardousApplication.class, args);
     }
 
